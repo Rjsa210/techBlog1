d4e817666a0f73a25f2cb9a98c1702c9139c734e
Reid Johnson <rjsa210@gmail.com>
2021-05-02 18:29:06 -0700

    commiting for review -RJ

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,7 +3,7 @@ const { User } = require('../../models');
 
 
 // signup
-
+// WHY DOESN'T THIS WORK
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
@@ -11,17 +11,28 @@ router.post('/', async (req, res) => {
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-
+      
       res.status(200).json(userData);
     });
 
   } catch (err) {
+    console.log(req.body);
     res.status(400).json(err);
   }
 });
 
 
-
+router.post('/login', async (req, res) => {
+  try{
+    const userData = await User.findOne({where: { username: req.body.username}});
+    if (!userData)  {
+      res.status(400).json({message: 'incorrect username or email'})
+    }
+  }
+  catch(err) {
+    res.status(500).json(err);
+  }
+})
 
 
 module.exports = router;
